40706045c72a7320076bea272d14349930e67480
ctemple <ctemple@21cn.com>
2022-10-17 10:39:19 +0800

    打印图片匹配distance

--- test_match_cv/main.py
+++ test_match_cv/main.py
@@ -31,10 +31,10 @@ def check_color(full_img, regn_img):
                 ++diff_num
     return diff_num < (GLOBAL_W * GLOBAL_H) * 0.1
 
-img1 = cv2.imread('match_full.png', 0)
-img2 = cv2.imread('match_regn.png', 0)
+img1 = cv2.imread('3.png', 0)
+img2 = cv2.imread('4.png', 0)
 
-print(type(img1))
+# print(type(img1))
 
 start_time = time.perf_counter_ns()
 #使用sift算法，计算特征向量与特征点
@@ -43,26 +43,25 @@ kp1, des1 = sift.detectAndCompute(img1, None)
 kp2, des2 = sift.detectAndCompute(img2, None)
 
 
-if len(kp1) or len(kp2) == 0:
-    print(check_color(img1, img2))
-    exit(0)
+# if len(kp1) or len(kp2) == 0:
+#     print(check_color(img1, img2))
+#     exit(0)
 
 # crossCheck表示两个特征点要互相匹，例如A中的第i个特征点与B中的第j个特征点最近的，并且B中的第j个特征点到A中的第i个特征点也是
 #NORM_L2: 归一化数组的(欧几里德距离)，如果其他特征计算方法需要考虑不同的匹配计算方式
 bf = cv2.BFMatcher(crossCheck=True)
 matches = bf.match(des1, des2)
 use_time = time.perf_counter_ns() - start_time
-print(use_time)
+print("use_time", use_time)
 
 
 #对点的距离的排序
 matches = sorted(matches, key=lambda x: x.distance)
-print(matches[0].distance)
+print("distance=",matches[0].distance)
 
 match_desc = kp1[matches[0].queryIdx].pt
-print(match_desc)
+print("pt=",match_desc)
 
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None,flags=2)
+#img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None,flags=2)
 #图片展示
-
-img_show('img3',img3)
+#img_show('img3',img3)
